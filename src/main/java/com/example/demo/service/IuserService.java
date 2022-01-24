e55e4d93b54d187c449a8ee246043bba0e18ecd9
nidhisingh2224 <tcslearning2422@gmail.com>
2022-01-24 12:09:54 +0530

    adding transactional annotation to post and delete method

--- src/main/java/com/example/demo/service/IuserService.java
+++ src/main/java/com/example/demo/service/IuserService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,11 @@ public class IuserService {
 	@Autowired // dependency injection
 	UserRepository userRepository;
 
+	@Transactional
 	public void saveUser(User user) {
 		// TODO Auto-generated method stub
 		userRepository.save(user);
+		throw new IllegalArgumentException();
 
 	}
 
@@ -27,7 +31,8 @@ public class IuserService {
 		// TODO Auto-generated method stub
 		return userRepository.findById(id);
 	}
-
+	
+    @Transactional
 	public void deleteUser(Integer id) {
 		// TODO Auto-generated method stub
 		userRepository.deleteById(id);
